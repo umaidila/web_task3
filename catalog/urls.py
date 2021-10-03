57640147a633fe70d10871a5c62234a23d89dc17
umaidila <frenirus125yandex.ru>
2021-10-03 23:26:07 +1000

    .

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
     url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    url(r'^allbooks/$', views.LoanedBooksByAllUsersListView.as_view(), name='all-borrowed')
 ]
