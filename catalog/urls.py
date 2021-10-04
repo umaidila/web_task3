69ece2f3afd31dea58c6f72ce99dec841f41f6fe
[umaidila] <[frenirus125@gmail.com]>
2021-10-04 17:00:51 +1000

    3 done

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
     url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    url(r'^allbooks/$', views.LoanedBooksByAllUsersListView.as_view(), name='all-borrowed')
+    url(r'^allbooks/$', views.LoanedBooksByAllUsersListView.as_view(), name='all-borrowed'),
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete')
 ]
